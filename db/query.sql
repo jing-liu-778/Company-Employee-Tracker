5217655308bc8069e054670a1eb04a57be60b8ea
Jing Liu <jingliu778@gmail.com>
2022-01-25 17:19:51 -0500

    create db query to show department, roles, employee

---
+++ db/query.sql
@@ -0,0 +1,18 @@
+-- show employee
+SELECT employee.id, 
+        employee.first_name, 
+        employee.last_name, 
+        roles.title, 
+        department.department_name AS department, 
+        roles.salary, 
+        CONCAT(manager.first_name, " ", manager.last_name) AS manager
+FROM employee
+LEFT JOIN roles ON employee.role_id = roles.id
+LEFT JOIN department ON roles.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id;
+
+-- show roles
+SELECT roles.id, roles.title,  department.department_name AS department, roles.salary
+FROM roles
+LEFT JOIN department ON roles.department_id = department.id;
+
