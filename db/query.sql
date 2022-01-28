b78613f8f25add74a211618f8d95a39754729b21
Jing Liu <jingliu778@gmail.com>
2022-01-28 13:44:37 -0500

    create view employee by manager and by department

--- db/query.sql
+++ db/query.sql
@@ -18,3 +18,19 @@ LEFT JOIN department ON roles.department_id = department.id;
 -- add a department
 
 INSERT INTO department(department_name) VALUES(?);
+
+---- view employee by department
+SELECT employee.first_name, 
+        employee.last_name, 
+        department.department_name AS department
+FROM employee 
+LEFT JOIN roles ON employee.role_id = roles.id 
+LEFT JOIN department ON roles.department_id = department.id;
+----- view employee by manager
+SELECT 
+        sub.id, 
+    sub.first_name ,
+    sub.last_name ,
+    CONCAT(sup.first_name, " ", sup.last_name) AS manager
+FROM employee sub JOIN employee sup ON sub.manager_id = sup.id;
+
