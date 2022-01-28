715c85bcee1ca9d207114e91d646d5fc37c155b1
Jing Liu <jingliu778@gmail.com>
2022-01-28 14:38:44 -0500

    create view budget, delete function

--- db/query.sql
+++ db/query.sql
@@ -19,18 +19,27 @@ LEFT JOIN department ON roles.department_id = department.id;
 
 INSERT INTO department(department_name) VALUES(?);
 
----- view employee by department
+-- view employee by department
 SELECT employee.first_name, 
         employee.last_name, 
         department.department_name AS department
 FROM employee 
 LEFT JOIN roles ON employee.role_id = roles.id 
-LEFT JOIN department ON roles.department_id = department.id;
------ view employee by manager
-SELECT 
+LEFT JOIN department ON roles.department_id = department.id
+GROUP BY department_name;
+--  view employee by manager
+ SELECT 
         sub.id, 
     sub.first_name ,
     sub.last_name ,
     CONCAT(sup.first_name, " ", sup.last_name) AS manager
-FROM employee sub JOIN employee sup ON sub.manager_id = sup.id;
+ FROM employee sub JOIN employee sup ON sub.manager_id = sup.id;
 
+-- view salary by department
+
+SELECT  department.id,
+        department.department_name as department, 
+        SUM(roles.salary) AS budget
+FROM  roles
+LEFT JOIN department on department.id = roles.department_id
+GROUP BY department.id;
