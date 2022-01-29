22aa90e10d26a0eeb60207dc05b8853ea29e31eb
Jing Liu <jingliu778@gmail.com>
2022-01-28 19:15:49 -0500

    update view employee by manager and department, create readme file

--- db/query.sql
+++ db/query.sql
@@ -25,8 +25,8 @@ SELECT employee.first_name,
         department.department_name AS department
 FROM employee 
 LEFT JOIN roles ON employee.role_id = roles.id 
-LEFT JOIN department ON roles.department_id = department.id
-GROUP BY department_name;
+LEFT JOIN department ON roles.department_id = department.id;
+
 --  view employee by manager
  SELECT 
         sub.id, 
@@ -44,4 +44,14 @@ FROM  roles
 LEFT JOIN department on department.id = roles.department_id
 GROUP BY department.id;
 -- view employee by manager
-SELECT DISTINCT m.id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e Inner JOIN employee m ON e.manager_id = m.id
+SELECT DISTINCT m.id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e Inner JOIN employee m ON e.manager_id = m.id;
+
+-- test
+SELECT employee.first_name, 
+        employee.last_name, 
+        department.department_name AS department
+FROM employee 
+
+LEFT JOIN roles ON employee.role_id = roles.id 
+LEFT JOIN department ON roles.department_id = department.id
+WHERE department_id=1;
